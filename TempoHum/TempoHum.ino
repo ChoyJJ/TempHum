24849e09f853fefc977538207d11435b8f5d6170
ChoyJJ <87667572+ChoyJJ@users.noreply.github.com>
2022-04-01 01:52:50 +0800

    Button and LCD display optimisation

--- TempoHum/TempoHum.ino
+++ TempoHum/TempoHum.ino
@@ -30,11 +30,12 @@ DHT dht(DHTPIN, DHTTYPE);
 WiFiManager wm;
 //WiFiServer server(80);
 //Setting pin for Reset, Enable, D4, D5,D6,D7
-//Pin connected D3, D4, D6, D7, D8, Tx(1), Change back to TX when Serial not in use
-LiquidCrystal lcd(0, 2, 12, 13, 15, 4);
+//Pin connected D3, D4, D6, D7, D8, Tx(1), Change back to TX when //Serial not in use
+LiquidCrystal lcd(0, 2, 12, 13, 15, 10);
 
 const char* UID = "123456";
 const char* WebURL = "https://tempo-hum-web-ui.vercel.app/";
+const String URLlink = "https://tempo-hum-web-ui.vercel.app/";
 char POSTURL[100];
 
 WiFiClientSecure wifiClient;
@@ -49,8 +50,8 @@ int prevTime, currentTime, duration, POSTprevTime;
 void setup() {
   //Start DHT
   dht.begin();
-  //Serial monitor for debugging
-  Serial.begin(9600);
+  ////Serial monitor for debugging
+  //Serial.begin(9600);
   //Set input pins for button at D0 and D1
   pinMode(BTN1, INPUT);
   pinMode(BTN2, INPUT);
@@ -58,7 +59,7 @@ void setup() {
   //Initialize the number of text and number of line
   lcd.begin(LCDTEXT, LCDLINE);
   lcd.print("Initialising");
-  Serial.println("Initialising");
+  //Serial.println("Initialising");
   //If possible, figure out how the secure network protocol work
   wifiClient.setInsecure();
   //Set as station to connect to WiFi
@@ -69,10 +70,10 @@ void setup() {
   //wm.resetSettings();
   //Start WiFi manager to connect to WiFi, if no WiFi go into Access Point
   //Access point started with Name and Password
-  res = wm.autoConnect("ESP32Test", "12345678abcdefg");
+  res = wm.autoConnect("TEMPOHUM", "SENSYNC+TEMPOHUM");
   //if WiFi Connection fails
   if (!res) {
-    Serial.println("Failed to connect");
+    //Serial.println("Failed to connect");
     lcd.clear();
     lcd.print("Failed to");
     lcd.setCursor(0, 1);
@@ -82,58 +83,74 @@ void setup() {
     lcd.print("Right: Retry");
     lcd.setCursor(0, 1);
     lcd.print("Left: Skip WiFi");
-    Serial.print("Right: Retry");
-    Serial.print("Left: Skip WiFi");
+    //Serial.print("Right: Retry");
+    //Serial.print("Left: Skip WiFi");
     prevTime = millis();
     //Hold when button is not clicked
     while (!digitalRead(BTN1) && !digitalRead(BTN2)) {
       currentTime = millis();
       duration = currentTime - prevTime;
+      //No input for 10 seconds, automatic skip wifi
       if (duration > 10000) {
-        Serial.println("Time out");
+        //Serial.println("Time out");
         Timeout = 1;
         break;
       }
+      //Prevent WDT from timing out
+      delay(10);
     }
-
-    if (digitalRead(BTN1) || Timeout == 1) {
+    while (digitalRead(BTN1) || digitalRead(BTN2)) {}
+    //Press Left button to restart the ESP
+    if (digitalRead(BTN1)) {
       lcd.clear();
       lcd.print("Restarting...");
-      Serial.println("Restarting");
+      //Serial.println("Restarting");
       ESP.restart();
     }
-    else if (digitalRead(BTN2)) {
+    //Press right button
+    else if (digitalRead(BTN2) || Timeout == 1) {
       REJWiFi = 1;
+      Timeout = 0;
       lcd.clear();
       lcd.print("Continue without");
       lcd.setCursor(0, 1);
       lcd.print("WiFi");
-      Serial.println("Continue Without WiFi");
-      delay(1000);
+      //Serial.println("Continue Without WiFi");
+
       //Set power saving mode without WiFi
     }
+
   }
+  //WiFi is connected, show that WiFi is connected
   else {
-    Serial.println("");
-    Serial.println("WiFi connected");
+    //Serial.println("");
+    //Serial.println("WiFi connected");
     lcd.clear();
     lcd.print("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
   }
+  delay(1000);
+  //SHow default setting
   lcd.clear();
-  lcd.print("5min interval");
+  lcd.print("Default interval");
   lcd.setCursor(0, 1);
-  lcd.print("Outdoor Use");
+  lcd.print("5min - indoor");
+  //Serial.println("5 min - default");
   prevTime = millis();
+  //Wait for 5 seconds or user input
   do {
-      currentTime = millis();
-      duration = currentTime - prevTime;
-      if (digitalRead(BTN1) || digitalRead(BTN2)) {
-        return;
-      }
-      delay(10);
-    } while (duration < 2000);
+    currentTime = millis();
+    duration = currentTime - prevTime;
+    //if user clicks on button, escape te loop and continue function
+    if (digitalRead(BTN1) || digitalRead(BTN2)) {
+      break;
+    }
+    delay(10);
+  } while (duration < 5000);
+  DHTSENSOR();
+  if (WiFi.status() == WL_CONNECTED) {
+    POSTREQ();
+  }
+  DHTLCDPRINT();
   POSTprevTime = millis();
 }
 
@@ -141,12 +158,10 @@ void loop() {
   do {
     LeftBTN = digitalRead(BTN1);
     RightBTN = digitalRead(BTN2);
-    delay(10);
-    Serial.println("Running" + String(LeftBTN) + String(RightBTN));
+    delay(1);
   } while (digitalRead(BTN1) || digitalRead(BTN2) && !(digitalRead(BTN1) && digitalRead(BTN2)) );
-  Serial.println("Test");
   if (WiFi.status() == WL_DISCONNECTED && REJWiFi == 0) {
-    Serial.println("WiFi Disconnected");
+    //Serial.println("WiFi Disconnected");
     lcd.clear();
     lcd.print("WiFi Disconnected");
     delay(1000);
@@ -154,9 +169,9 @@ void loop() {
     lcd.print("Starting WiFi");
     lcd.setCursor(0, 1);
     lcd.print("Manager");
-    res = wm.autoConnect("ESP32Test", "12345678abcdefg");
+    res = wm.autoConnect("TEMPOHUM", "SENSYNC+TEMPOHUM");
     if (!res) {
-      Serial.println("Failed to connect");
+      //Serial.println("Failed to connect");
       lcd.clear();
       lcd.print("Failed to");
       lcd.setCursor(0, 1);
@@ -166,13 +181,23 @@ void loop() {
       lcd.print("Right for rst");
       lcd.setCursor(0, 1);
       lcd.print("Left for skip");
-      Serial.print("Right for rst");
-      Serial.print("Left for skip");
-      while (digitalRead(BTN1) == 0 && digitalRead(BTN2) == 0) {}
+      //Serial.print("Right for rst");
+      //Serial.print("Left for skip");
+      while (digitalRead(BTN1) == 0 && digitalRead(BTN2) == 0) {
+        currentTime = millis();
+        duration = currentTime - prevTime;
+        //No input for 10 seconds, automatic skip wifi
+        if (duration > 10000) {
+          //Serial.println("Time out");
+          Timeout = 1;
+          break;
+        }
+        delay(10);
+      }
       if (digitalRead(BTN1)) {
         lcd.clear();
         lcd.print("Retry");
-        Serial.println("Retry");
+        //Serial.println("Retry");
         return;
       }
       else if (digitalRead(BTN2)) {
@@ -181,64 +206,50 @@ void loop() {
         lcd.print("Continue without");
         lcd.setCursor(0, 1);
         lcd.print("WiFi");
-        Serial.println("Continue Without WiFi");
+        //Serial.println("Continue Without WiFi");
         delay(1000);
+        Timeout = 0;
         //Set power saving mode without WiFi
       }
     }
     else {
       //if you get here you have connected to the WiFi
-      Serial.println("");
-      Serial.println("WiFi connected");
+      //Serial.println("");
+      //Serial.println("WiFi connected");
       lcd.clear();
       lcd.print("WiFi connected");
-      Serial.println("IP address: ");
-      Serial.println(WiFi.localIP());
+      //Serial.println("IP address: ");
+      //Serial.println(WiFi.localIP());
     }
+    delay(1000);
   }
-  if (BTNL == 0 && BTNR == 0) {
-    Serial.println("Press a Button");
-    lcd.clear();
-    lcd.print("Left");
-    lcd.setCursor(0, 1);
-    lcd.print("Set Interval");
-    Serial.println("Left: Set Interval");
-    prevTime = millis();
-    do {
-      currentTime = millis();
-      duration = currentTime - prevTime;
-      if (digitalRead(BTN1) || digitalRead(BTN2)) {
-        return;
-      }
-      delay(10);
-    } while (duration < 2000);
-    lcd.clear();
-    lcd.print("Right");
-    lcd.setCursor(0, 1);
-    lcd.print("Menu");
-    Serial.println("Right Menu");
-    prevTime = millis();
-    do {
-      currentTime = millis();
-      duration = currentTime - prevTime;
-      if (digitalRead(BTN1) || digitalRead(BTN2)) {
-        return;
-      }
-      delay(10);
-    } while (duration < 2000);
-  }
-  Serial.println(String(LeftBTN) + String(RightBTN));
+
   if (LeftBTN && RightBTN) {
-    Serial.println("Resetting for WiFi connection");
+    //Serial.println("Resetting for WiFi connection");
     lcd.clear();
     lcd.print("Reconnect WiFi");
+    lcd.setCursor(0, 1);
+    lcd.print("Hold to reset");
     //Restart ESP WiFi functions
     REJWiFi = 0;
     prevTime = millis();
+    do {
+      currentTime = millis();
+      duration = currentTime - prevTime;
+      delay(10);
+      if (duration > 5000)
+      {
+        wm.resetSettings();
+        lcd.clear();
+        lcd.print("WiFi reset");
+        delay(1000);
+      }
+    } while (digitalRead(BTN1) && digitalRead(BTN2));
+    DHTLCDPRINT();
     return;
   }
   else if (LeftBTN) {
-    Serial.println("Left");
+    //Serial.println("Left");
     prevTime = millis();
     do {
       currentTime = millis();
@@ -253,67 +264,87 @@ void loop() {
           lcd.print("1min interval");
           lcd.setCursor(0, 1);
           lcd.print("Outdoor Use");
-          POST_interval = 5 * 1000;
-          Serial.println(POST_interval);
+          POST_interval = 60 * 1000;
+          //Serial.println("1 min - Indoor Use");
           prevTime = currentTime;
-          POSTprevTime = millis();
         }
         else {
           lcd.clear();
           lcd.print("5min interval");
           lcd.setCursor(0, 1);
-          lcd.print("Outdoor Use");
+          lcd.print("Indoor Use");
           //Set to 5 minute interval
-          POST_interval = 5*60*1000;
+          //Serial.println("5 min - Indoor Use");
+          POST_interval = 5 * 60 * 1000;
           BTNL = 0;
           prevTime = currentTime;
         }
       }
       duration = currentTime - prevTime;
       delay(10);
-    } while (duration < 5000);
+    } while (!digitalRead(BTN2) && duration < 5000);
+    prevTime = currentTime;
+    DHTLCDPRINT();
+    while(digitalRead(BTN1) || digitalRead(BTN2)){delay(1);}
+  }
+  else if (RightBTN) {
+    //Serial.println("Right");
+    BTNR++;
+    if (BTNR == 1) {
+      lcd.clear();
+      lcd.print("URL: ");
+      //Serial.println("URL:");
+      //Serial.println(WebURL);
+      URLP = 1;
+      prevTime = millis();
+      int n = 0, c = 0;
+      int l = URLlink.length();
+      do {
+        //        if(n/800 > l-15){n=0;}
+        if (n >= 500) {
+          if (c > l) {
+            c = 0;
+          }
+          lcd.setCursor(0, 1);
+          lcd.print(URLlink.substring(c, c + 15));
+          n = 0;
+          c++;
+        }
+        currentTime = millis();
+        duration = currentTime - prevTime;
+        delay(1);
+        n++;
+      } while (!digitalRead(BTN1) && !digitalRead(BTN2) && duration < 15000 );
+    }
+    else if (BTNR == 2) {
+      lcd.clear();
+      lcd.print("UID: ");
+      lcd.setCursor(0, 1);
+      lcd.print(UID);
+      //Serial.println("UID:");
+      //Serial.println(UID);
+      UIDP = 1;
+      BTNR = 0;
+      prevTime = millis();
+      do {
+        currentTime = millis();
+        duration = currentTime - prevTime;
+        delay(1);
+      } while (!digitalRead(BTN1) && !digitalRead(BTN2) && duration < 10000 );
+    }
+    DHTLCDPRINT();
+    while(digitalRead(BTN1) || digitalRead(BTN2)){delay(1);}
+  }
+  currentTime = millis();
+  duration = currentTime - POSTprevTime;
+  if (duration > POST_interval && POST_interval > 0) {
     DHTSENSOR();
     if (WiFi.status() == WL_CONNECTED) {
       POSTREQ();
     }
     DHTLCDPRINT();
-    prevTime = currentTime;
-    return;
-  }
-  else if (RightBTN) {
-    Serial.println("Right");
-    BTNR++;
-    if (BTNR == 1) {
-      Serial.println("URL:");
-      Serial.println(WebURL);
-      URLP = 1;
-      return;
-    }
-    else if (BTNR == 2) {
-      Serial.println("UID:");
-      Serial.println(UID);
-      UIDP = 1;
-      return;
-    }
-    else if (BTNR > 2) {
-      BTNR = 0;
-      DHTLCDPRINT();
-      prevTime = currentTime;
-      return;
-    }
-  }
-  currentTime = millis();
-  duration = currentTime - POSTprevTime;
-  Serial.println(duration);
-  if(duration > POST_interval && POST_interval > 0){
-    DHTSENSOR();
-    if (WiFi.status() == WL_CONNECTED){
-      POSTREQ();
-    }
-    DHTLCDPRINT();
     POSTprevTime = currentTime;
   }
-  delay(1000);
 }
 
 
@@ -323,23 +354,25 @@ void DHTLCDPRINT() {
   lcd.print("Temp: " + String(t / 100.0) + char(223) + "C ");
   lcd.setCursor(0, 1);
   lcd.print("Humid: " + String(h / 100.0) + "% ");
-  Serial.println("Temp: " + String(t / 100.0) + "°C ");
-  Serial.println("Humid: " + String(h / 100.0) + "% ");
+  //Serial.println("Temp: " + String(t / 100.0) + "°C ");
+  //Serial.println("Humid: " + String(h / 100.0) + "% ");
   delay(100);
 }
 
 void DHTSENSOR() {
   float H = dht.readHumidity();
   float T = dht.readTemperature();
-  h = (int)H * 100.0;
-  t = (int)T * 100.0;
+  H = H * 100.0;
+  T = T * 100.0;
+  h=(int)H;
+  t=(int)T;
 }
 
 void POSTREQ() {
   String response;
   HTTPClient http;
   sprintf(POSTURL, "%slist/sensor/%s.json", WebURL, UID);
-  Serial.println("Sending data");
+  //Serial.println("Sending data");
   lcd.clear();
   lcd.print("Sending data");
   http.begin(wifiClient, POSTURL);
@@ -348,17 +381,19 @@ void POSTREQ() {
   dat["UID"] = UID;
   dat["Temperature"] = t / 100.0;
   dat["Humidity"] = h / 100.0;
-  serializeJson(dat, postjson);
-  Serial.println(POSTURL + postjson);
+  //SerializeJson(dat, postjson);
+  //Serial.println(POSTURL + postjson);
   http.addHeader("Content-Type", "application/json");
   int httpResponseCode = http.POST(postjson);
-  Serial.println("HttpResponse=" + String(httpResponseCode));
+  //Serial.println("HttpResponse=" + String(httpResponseCode));
   response = http.getString();
-  Serial.println(response);
+  //Serial.println(response);
   if (httpResponseCode >= 200 && httpResponseCode < 300) {
     lcd.clear();
     lcd.print("Data sent");
-    Serial.println("Data sent");
+    //Serial.println("Data sent");
+
   }
   http.end();
+  delay(2000);
 }
